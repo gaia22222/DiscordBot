1f464decd23359ee9317e789fa80266ac0514d16
gaia22222 <gaialeung123@gmail.com>
2025-01-01 00:55:11 +0800

    Update AI to conversation mode

--- cogs/Chat.py
+++ cogs/Chat.py
@@ -1,85 +1,54 @@
 import discord
 from discord.ext import commands
 from core.classes import Cog_Extension
-import string
-import random
-import typing
 import os
-#from groq import Groq
 import google.generativeai as genai
 
 
 class Chat(Cog_Extension):
   
-  @commands.command() 
-  async def ask(self,ctx):
-    genai.configure(api_key=os.environ.get('GeminiAI'))
-    try:
-      async with ctx.channel.typing():
-        promp = ctx.message.content.replace('!ask','')
-        model = genai.GenerativeModel("gemini-1.5-flash")
-        response = model.generate_content(promp,
-            generation_config = genai.GenerationConfig(
-            max_output_tokens=3000,
-            temperature=0.7,
-            ))
-        #print(response.text)
-        await ctx.channel.send(response.text)
-    except Exception as e:
-      await ctx.channel.send("An error occurred during the request")
-      print("Something went wrong")
-      print(e)
-  '''
-  @commands.Cog.listener()
-  async def on_message(self,msg):
-    if(msg.author == self.bot.user):
-      return
+    def __init__(self, bot):
+        super().__init__(bot)
+        genai.configure(api_key=os.environ.get('GeminiAI'))
+        self.model = genai.GenerativeModel("gemini-1.5-flash")
+        self.conversation = self.model.start_chat()
+        self.designated_channel_id = None
 
-    if not((msg.channel.id != 1079770157414625370) ^ (msg.channel.id != 540973843775488012)):
-      return
-    #ori
-    #if (msg.channel.id != 1079770157414625370):
-      #return
-    #本bot
-    #if (msg.channel.id != 540973843775488012):
-      #return
-    if(msg.content[0] == '!'):
-      return
-    config = {
-      "session_token": os.getenv('Chat_Token')
-    }
-    chatbot = Chatbot(config,conversation_id=None)
-    try:
-        async with msg.channel.typing():
-            prompt = msg.content
-            for data in chatbot.ask(prompt):
-              response = data["message"]
-            await msg.channel.send(response)
-            
-    except Exception as e:
-        await msg.channel.send("An error occurred during the request")
-        print("Something went wrong")
-        print(e)
-      
-  @commands.command()
-  async def ask(ctx, *arg):
-    config = {
-      "session_token": os.getenv('Chat_Token')
-    }
-    chatbot = Chatbot(config,conversation_id=None)
-    try:
-        async with ctx.typing():
-            prompt = ctx.message.content
-            for data in chatbot.ask(prompt):
-              response = data["message"]
-              
-            await ctx.send(response)
-            
-    except Exception as e:
-        await ctx.send("An error occurred during the request")
-        print("Something went wrong")
-        print(e)
-        '''
+    @commands.hybrid_command(brief='/ask', description='Ask to AI')
+    async def ask(self, ctx, *, message):
+        if self.designated_channel_id != ctx.channel.id:
+            old_channel = self.bot.get_channel(self.designated_channel_id)  # 获取之前的频道
+            if old_channel: # 檢查舊頻道是否存在 (可能已被刪除)
+                await old_channel.send(f'聊天機器人可能已在其他頻道啟動，現在的記憶記錄已清除。')
+
+            self.designated_channel_id = ctx.channel.id
+            self.conversation = self.model.start_chat()
+            await ctx.send('正在此頻道啟動新的對話，之前的記憶記錄已清除。')
+        try:
+            async with ctx.channel.typing():
+                response = self.conversation.send_message(message,
+                  generation_config = genai.GenerationConfig(
+                  max_output_tokens=1000,
+                  temperature=0.3,
+                ))
+                # response = self.conversation.generate_content(
+                #     generation_config=genai.GenerationConfig(
+                #         max_output_tokens=3000,
+                #         temperature=0.3,
+                #     )
+                # )
+                await ctx.channel.send(response.text)
+        except Exception as e:
+            await ctx.channel.send("An error occurred during the request")
+            print("Something went wrong")
+            print(e)
+
+    @commands.command()
+    @commands.is_owner()
+    async def reset_chat(self, ctx):
+        self.conversation = None
+        self.designated_channel_id = None # 重置 designated_channel_id
+        await ctx.send("現在頻道聊天記錄已重置。")
 
 async def setup(bot):
     await bot.add_cog(Chat(bot))
